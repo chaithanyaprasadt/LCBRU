1d0e3545fc934fe6a51ebed4c69aafb8dd1694c0
chaithanyaprasadt <iahoumengnas822@163.com>
2021-06-14 21:01:25 +0800

    Move input and transform to files

--- microMedicalSpirometerXmlToCsv.py
+++ microMedicalSpirometerXmlToCsv.py
@@ -7,24 +7,15 @@ COLUMN_UHL_SYSTEM_NUMBER = 'UhlSystemNumber'
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('inputfile', nargs='?')
-#  parser.add_argument('inputfile', nargs=1)
+  parser.add_argument('inputfile', nargs=1)
   parser.add_argument("-o", "--outputfilename", nargs='?', help="Output filename", default="output.csv")
   args = parser.parse_args()
 
-  transformXmlToCsv(args.inputfile)
+  transformXmlToCsv(args.inputfile[0])
 
 def transformXmlToCsv(inputfile):
-  xslt = ET.XML('''\
-     <xsl:stylesheet version="1.0"
-        xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-        <xsl:output method="text"/>
-        <xsl:template match="/">
-            <xsl:value-of select="foo" />
-        </xsl:template>
-    </xsl:stylesheet>''')
-  dom = ET.XML('''<?xml version="1.0"?>
-      <foo>Text</foo>''')
+  xslt = ET.parse('transform.xslt')
+  dom = ET.parse(inputfile)
   transform = ET.XSLT(xslt)
   print(str(transform(dom)))
 
